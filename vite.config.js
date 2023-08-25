8a0f548dcdbb223875a06c609e244166269b937c
PollyPO Lee <polly0215yu@gmail.com>
2023-08-25 16:55:14 +0800

    安裝 React router 套件，並套用至現有架構內 替元件引入路徑加上 .jsx 調整 vite.config 的 alias 調整 HashRouter 位置 調整 github actions 加入 cache 嘗試改變 env path 檔案名稱大小寫問題

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite';
+import { resolve } from "path"; // 主要用在 alias 文件路徑别名
+import react from '@vitejs/plugin-react-swc';
+
+// const isProduction = process.env.NODE_ENV === 'production';
 
 // https://vitejs.dev/config/
+// export default defineConfig(({mode}) => {
+//   const env = loadEnv(mode, process.cwd());
+
+//   return {
+//     base: env.VITE_BASE_URL,
+//     plugins: [react()],
+//     server: {
+//       watch: {
+//         usePolling: true,
+//       },
+//       host: true, // Docker mapping port 所需
+//       strictPort: true,
+//       port: 8080, // Docker export 出來的 port
+//     },
+//     resolve: {
+//       alias: {
+//         '@': resolve('./src'),
+//         '@weekOne': resolve('./src/week01/'),
+//         '@weekTwo': resolve('./src/week02/'),
+//       },
+//     },
+//   }
+// });
+
 export default defineConfig({
   base: './',
   plugins: [react()],
@@ -12,7 +39,14 @@ export default defineConfig({
     host: true, // Docker mapping port 所需
     strictPort: true,
     port: 8080, // Docker export 出來的 port
-  }
+  },
+  resolve: {
+    alias: {
+      '@': resolve('./src'),
+      '@weekOne': resolve('./src/week01/'),
+      '@weekTwo': resolve('./src/week02/'),
+    },
+  },
 })
 
 // 前端顯示的 URL: localhost:3000 (3000 port 可以自己指定)
